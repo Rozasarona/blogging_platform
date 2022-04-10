bf6459c811b0ad8775b9c8139b17e84dfe8f13d2
Roza Sarona <svetlanam300@gmail.com>
2022-04-10 19:36:51 +0300

    SignInForm - fixing

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,9 +8,8 @@ function Header () {
         <header className="header">
             <Link to='/' className="logo">Realworld Blog</Link>
             <div className="options">
-                <a href="://localhost:3000" className="signin_btn">
-                    Sign In
-                </a>
+                {/*<a href="://localhost:3000" className="signin_btn">Sign In</a> */}
+                <Link to='/sign-in' className="signin_btn">Sign In</Link>
                 {/* <a href="://localhost:3000" className="signup_btn">Sign Up</a> */}
                 <Link to='/sign-up' className="signup_btn">Sign Up</Link>
             </div>
